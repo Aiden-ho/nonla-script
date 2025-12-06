52ba07cde0e32dd1a306442dff9321cfcbb5478f
Ho Tuan Kiet <aiden.hokiet@gmail.com>
2025-12-07 01:22:18 +0700

    add context variable

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 import { GsapSetup } from "./utils/gsapConfig.js";
-import { heroSectionInit } from "./modules/heroSection.js";
+import { SlittingHeroSectionInit } from "./modules/slittingHeroSection.js";
 import { getViewportName, checkMotionReduce } from "./utils/helpers.js";
 
 function initPage() {
   const viewportName = getViewportName();
   const isMotionReduce = checkMotionReduce();
+  const context = { viewportName, isMotionReduce };
 
   GsapSetup();
-  heroSectionInit();
+  SlittingHeroSectionInit(context);
 }
 
 document.addEventListener("DOMContentLoaded", () => initPage());
