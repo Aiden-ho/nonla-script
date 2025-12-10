2defb1e69e2c0329e3b49963524df84f9a094850
Ho Tuan Kiet <aiden.hokiet@gmail.com>
2025-12-10 14:40:56 +0700

    add two animation

---
+++ modules/horizonScrollAbout.js
@@ -0,0 +1,132 @@
+import { BREAKPOINT, DIRECTION } from "../utils/constant.js";
+
+function getSlideScrollElements() {
+  const tracker = document.querySelector('[data-horizon="tracker"]');
+  const wrapper = document.querySelector('[data-horizon="wrapper"]');
+  const progress = document.querySelector('[data-horizon="progress"]');
+  const progress_value = progress.querySelector("div");
+
+  if (!tracker || !wrapper || !progress || !progress_value) return null;
+
+  return { tracker, wrapper, progress_value };
+}
+
+function getScrollAmount(direc, wrapper, tracker) {
+  const isHorizon = direc === DIRECTION.HORIZON;
+
+  const wrapperSize = isHorizon ? wrapper.clientWidth : wrapper.clientHeight;
+  const trackSize = isHorizon ? tracker.scrollWidth : tracker.scrollHeight;
+
+  const diff = trackSize - wrapperSize;
+
+  if (diff <= 0) return 0;
+
+  return -diff; // âm để slide theo chiều ngược scroll
+}
+
+// Make animation functions
+function createSlideScrollAnimation({
+  direc = DIRECTION.HORIZON,
+  scrub = 2,
+  ease = "none",
+} = {}) {
+  const els = getSlideScrollElements();
+  if (!els) return;
+
+  const { tracker, wrapper, progress_value } = els;
+
+  const isHorizon = direc === DIRECTION.HORIZON;
+  const axis = isHorizon ? "x" : "y";
+
+  const tl = gsap.timeline({
+    defautls: {
+      ease, // make all tweens use a ease of none, feels nicer with working with scrub
+    },
+    scrollTrigger: {
+      trigger: wrapper,
+      start: "top top",
+      end: () => `+=${getScrollAmount(direc, wrapper, tracker) * -1}`,
+      pin: true,
+      scrub,
+    },
+  });
+
+  return tl
+    .to(tracker, {
+      [axis]: getScrollAmount(direc, wrapper, tracker),
+    })
+    .from(
+      progress_value,
+      {
+        scaleX: 0,
+        transformOrigin: "center left",
+      },
+      "<"
+    );
+}
+
+function applyReducedMotionHorizonScroll({ direc = DIRECTION.VERTICAL }) {
+  const els = getSlideScrollElements();
+  if (!els) return;
+
+  const { tracker, wrapper, progress_value } = els;
+
+  const isHorizon = direc === DIRECTION.HORIZON;
+  const axis = isHorizon ? "x" : "y";
+
+  function getScrollAmount() {
+    let wrapperSize = isHorizon ? wrapper.clientWidth : wrapper.clientHeight;
+    let trackSize = isHorizon ? tracker.scrollWidth : tracker.scrollHeight;
+    return -(trackSize - wrapperSize);
+  }
+
+  ScrollTrigger.create({
+    trigger: wrapper,
+    start: "top top",
+    end: () => `+=${getScrollAmount(direc, wrapper, tracker) * -1}`,
+    pin: true,
+    scrub: true,
+    onUpdate: ({ progress }) => {
+      const amount = getScrollAmount(direc, wrapper, tracker);
+
+      gsap.set(tracker, {
+        [axis]: amount * progress,
+      });
+
+      // progress bar scale theo progress
+      gsap.set(progress_value, {
+        scaleX: progress,
+        transformOrigin: "center left",
+      });
+    },
+  });
+}
+
+// Strategies functions
+function mobileConfig() {
+  createSlideScrollAnimation({ direc: DIRECTION.VERTICAL });
+}
+
+function desktopConfig() {
+  createSlideScrollAnimation();
+}
+
+const AnimationStrategies = {
+  [BREAKPOINT.MOBILE]: mobileConfig,
+  [BREAKPOINT.TABLET]: desktopConfig,
+  [BREAKPOINT.SMALL_DESKTOP]: desktopConfig,
+  [BREAKPOINT.LARGE_DESKTOP]: desktopConfig,
+};
+
+export function horizonScrollAbout(context) {
+  const { viewportName, isMotionReduced } = context;
+
+  if (isMotionReduced) {
+    applyReducedMotionHorizonScroll();
+    return;
+  }
+
+  const animaiton = AnimationStrategies[viewportName]();
+  if (!animaiton) return;
+  animaiton();
+}
---
+++ modules/layerStackTopBottom.js
@@ -0,0 +1,86 @@
+import { BREAKPOINT } from "../utils/constant.js";
+
+// Make animation functions
+function createLayerStackAnimation({
+  duration = 0.5,
+  ease = "power2.out",
+  start = "top 50%",
+  end = "top top",
+  scrub = 1,
+  filter = "brightness(50%) blur(10px)",
+  scale = 0.98,
+} = {}) {
+  const topLayer = document.querySelector('[data-layer="top"]');
+  const bottomLayer = document.querySelector('[data-layer="bottom"]');
+
+  if (!topLayer || !bottomLayer) return;
+
+  return gsap.to(topLayer, {
+    ease,
+    scale,
+    duration,
+    startAt: { filter: "brightness(100%) blur(0px)" },
+    filter,
+    immediateRender: false,
+    duration,
+    scrollTrigger: {
+      trigger: bottomLayer,
+      start,
+      end,
+      scrub,
+    },
+  });
+}
+
+function applyReducedMotionLayerStack() {
+  const topLayer = document.querySelector('[data-layer="top"]');
+  const bottomLayer = document.querySelector('[data-layer="bottom"]');
+
+  if (!topLayer || !bottomLayer) return;
+
+  return ScrollTrigger.create({
+    trigger: ".about_section",
+    start: bottomLayer,
+    once: true,
+    onEnter: () => {
+      gsap.to(topLayer, {
+        duration: 0.2,
+        ease: "power1.out",
+        filter: "brightness(80%)",
+        scale: 0.98,
+      });
+    },
+  });
+}
+
+// Strategies functions
+function mobileConfig() {
+  createLayerStackAnimation({
+    filter: "brightness(70%) blur(4px)",
+    scale: 0.98,
+  });
+}
+
+function desktopConfig() {
+  createLayerStackAnimation();
+}
+
+const AnimationStrategies = {
+  [BREAKPOINT.MOBILE]: mobileConfig,
+  [BREAKPOINT.TABLET]: desktopConfig,
+  [BREAKPOINT.SMALL_DESKTOP]: desktopConfig,
+  [BREAKPOINT.LARGE_DESKTOP]: desktopConfig,
+};
+
+export function layerStackTopBottom(context) {
+  const { viewportName, isMotionReduced } = context;
+
+  if (isMotionReduced) {
+    applyReducedMotionLayerStack();
+    return;
+  }
+
+  const animaiton = AnimationStrategies[viewportName]();
+  if (!animaiton) return;
+  animaiton();
+}
