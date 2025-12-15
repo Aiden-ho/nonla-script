febd67dac62928a77f8783f117d4d5d01c26f8f5
Ho Tuan Kiet <aiden.hokiet@gmail.com>
2025-12-16 01:37:24 +0700

    add VIEWPORTRULES for  concept match media

--- utils/constant.js
+++ utils/constant.js
@@ -9,3 +9,26 @@ export const DIRECTION = {
   VERTICAL: "vertical",
   HORIZON: "horizon",
 };
+
+export const VIEWPORTRULES = [
+  {
+    name: BREAKPOINT.MOBILE,
+    query: "(max-width: 767px)",
+    condition: (width) => width <= 767,
+  },
+  {
+    name: BREAKPOINT.TABLET,
+    query: "(max-width: 991px)",
+    condition: (width) => width <= 991,
+  },
+  {
+    name: BREAKPOINT.SMALL_DESKTOP,
+    query: "(min-width: 1024px) and (max-width: 1440px)",
+    condition: (width) => 1024 >= width <= 1440,
+  },
+  {
+    name: BREAKPOINT.LARGE_DESKTOP,
+    query: "(min-width: 1441px)",
+    condition: (width) => width >= 1441,
+  },
+];
