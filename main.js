67926bc6c0eaa7c771473ef98fde0e9c3b279685
Ho Tuan Kiet <aiden.hokiet@gmail.com>
2025-12-16 01:36:38 +0700

    change concept to matchMedia

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
 import { GsapSetup } from "./utils/gsapConfig.js";
-import { SlittingHeroSectionInit } from "./modules/slittingHeroSection.js";
-import { getViewportName, checkMotionReduce } from "./utils/helpers.js";
+import { checkMotionReduce } from "./utils/helpers.js";
+import { VIEWPORTRULES } from "./utils/constant.js";
+import { slittingHeroSectionInit } from "./modules/slittingHeroSection.js";
+import { layerStackTopBottom } from "./modules/layerStackTopBottom.js";
+import { horizonScrollAbout } from "./modules/horizonScrollAbout.js";
+import { wheelRotateMomentSectionInit } from "./modules/wheelRotateMoment.js";
+import { initMomentsRope } from "./modules/calcRolePosition.js";
+
+const animationModules = [
+  slittingHeroSectionInit,
+  layerStackTopBottom,
+  horizonScrollAbout,
+  wheelRotateMomentSectionInit,
+];
 
 function initPage() {
-  const viewportName = getViewportName();
-  const isMotionReduce = checkMotionReduce();
-  const context = { viewportName, isMotionReduce };
-
   GsapSetup();
-  SlittingHeroSectionInit(context);
+  initMomentsRope();
+  const isMotionReduce = checkMotionReduce();
+  const mm = gsap.matchMedia();
+
+  VIEWPORTRULES.forEach((rule) => {
+    mm.add(rule.query, () => {
+      const ctx = gsap.context(() => {
+        const globalConfig = {
+          viewportName: rule.name,
+          isMotionReduce,
+        };
+
+        animationModules.forEach((initModule) => {
+          initModule(globalConfig);
+        });
+      });
+
+      return () => {
+        ctx.revert(); // kill tween, ScrollTrigger, clear inline styles
+      };
+    });
+  });
 }
 
 document.addEventListener("DOMContentLoaded", () => initPage());
