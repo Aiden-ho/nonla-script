6e7f409272a437472fa6efc68fa2acb5a97f5c5a
Ho Tuan Kiet <aiden.hokiet@gmail.com>
2025-12-23 23:54:57 +0700

    update main

--- main.js
+++ main.js
@@ -1,22 +1,62 @@
 import { GsapSetup } from "./utils/gsapConfig.js";
-import { checkMotionReduce, getViewportRule } from "./utils/helpers.js";
+import { checkMotionReduce, createRafDebouncer } from "./utils/helpers.js";
 import { VIEWPORTRULES } from "./utils/constant.js";
+import {
+  triggerPlayVideo,
+  createResizeObserver,
+} from "./utils/observeHelper.js";
+import { STORE, updateViewport } from "./utils/globalStore.js";
 import { slittingHeroSectionInit } from "./modules/slittingHeroSection.js";
-import { layerStackTopBottom } from "./modules/layerStackTopBottom.js";
+import { layerStackTopBottomInit } from "./modules/layerStackTopBottom.js";
 import { horizonScrollAboutInit } from "./modules/horizonScrollAbout.js";
 import { wheelRotateMomentSectionInit } from "./modules/wheelRotateMoment.js";
 import { initMomentsRope } from "./modules/calcRolePosition.js";
+import { expandVideoSectionInit } from "./modules/expandVideosection.js";
+import { drawerMaterialsInit } from "./modules/drawerMaterialsSection.js";
+import { VariantSectionInit } from "./modules/3DvariantsSection.js";
 
 const animationModules = [
   slittingHeroSectionInit,
-  layerStackTopBottom,
+  layerStackTopBottomInit,
   horizonScrollAboutInit,
   wheelRotateMomentSectionInit,
+  expandVideoSectionInit,
+  drawerMaterialsInit,
+  VariantSectionInit,
 ];
+
+const requestSTRefresh = createRafDebouncer(() => ScrollTrigger.refresh());
+
+function initOnce() {
+  //observer resize
+  createResizeObserver(document.documentElement, () => {
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+
+    if (w === STORE.VW && h === STORE.VH) return;
+
+    updateViewport();
+    requestSTRefresh();
+  });
+
+  //watch load img
+  document.addEventListener(
+    "load",
+    (e) => {
+      if (e.target?.tagName === "IMG") {
+        requestSTRefresh();
+      }
+    },
+    true
+  );
+}
 
 function initPage() {
   GsapSetup();
+  initOnce();
   initMomentsRope();
+  triggerPlayVideo();
+  updateViewport();
   const isMotionReduce = checkMotionReduce();
   const mm = gsap.matchMedia();
 
@@ -38,6 +78,7 @@ function initPage() {
       };
     });
   });
+  requestSTRefresh();
 }
 
 document.addEventListener("DOMContentLoaded", () => initPage());
